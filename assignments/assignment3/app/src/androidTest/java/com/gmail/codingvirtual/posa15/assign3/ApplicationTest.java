ba15ffca894fe370effddad2ff62ea5d9f77607e
Greg Palen <codingvirtual@gmail.com>
2015-04-21 09:17:09 -0500

    Initial commit of fresh assignment files

---
+++ assignments/assignment3/app/src/androidTest/java/com/gmail/codingvirtual/posa15/assign3/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.gmail.codingvirtual.posa15.assign3;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
