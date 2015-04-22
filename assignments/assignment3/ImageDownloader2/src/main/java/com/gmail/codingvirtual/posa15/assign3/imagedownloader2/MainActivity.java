29023378774954c0a01629879f63edeb55f3e7de
Greg Palen <codingvirtual@gmail.com>
2015-04-22 09:40:35 -0500

    copied files in from Assignment 1 as a starting point.

--- assignments/assignment3/ImageDownloader2/src/main/java/com/gmail/codingvirtual/posa15/assign3/imagedownloader2/MainActivity.java
+++ assignments/assignment3/ImageDownloader2/src/main/java/com/gmail/codingvirtual/posa15/assign3/imagedownloader2/MainActivity.java
@@ -1,39 +1,208 @@
 package com.gmail.codingvirtual.posa15.assign3.imagedownloader2;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.IBinder;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.URLUtil;
+import android.widget.EditText;
+import android.widget.Toast;
 
+/**
+ * A main Activity that prompts the user for a URL to an image and
+ * then uses Intents and other Activities to download the image and
+ * view it.
+ */
+public class MainActivity extends LifecycleLoggingActivity {
+    /**
+     * Debugging tag used by the Android logger.
+     */
+    private final String TAG = getClass().getSimpleName();
 
-public class MainActivity extends ActionBarActivity {
+    /**
+     * A value that uniquely identifies the request to download an
+     * image.
+     */
+    private static final int DOWNLOAD_IMAGE_REQUEST = 1;
 
+    /**
+     * EditText field for entering the desired URL to an image.
+     */
+    private EditText mUrlEditText;
+
+    /**
+     * URL for the image that's downloaded by default if the user
+     * doesn't specify otherwise.
+     */
+    private Uri mDefaultUrl =
+        Uri.parse("http://www.dre.vanderbilt.edu/~schmidt/robot.png");
+
+    /**
+     * Hook method called when a new instance of Activity is created.
+     * One time initialization code goes here, e.g., UI layout and
+     * some class scope variable initialization.
+     *
+     * @param savedInstanceState object that contains saved state information.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Always call super class for necessary
+        // initialization/implementation.
+        // @@ DONE -- you fill in here.
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Set the default layout.
+        // @@ DONE -- you fill in here.
+        setContentView(R.layout.main_activity);
+
+        // Cache the EditText that holds the urls entered by the user
+        // (if any).
+        // @@ DONE -- you fill in here.
+        mUrlEditText = (EditText) findViewById(R.id.url);
     }
 
+    /**
+     * Called by the Android Activity framework when the user clicks
+     * the "Find Address" button.
+     *
+     * @param view The view.
+     */
+    public void downloadImage(View view) {
+        try {
+            // Hide the keyboard.
+            hideKeyboard(this,
+                         mUrlEditText.getWindowToken());
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+            // Call the makeDownloadImageIntent() factory method to
+            // create a new Intent to an Activity that can download an
+            // image from the URL given by the user.  In this case
+            // it's an Intent that's implemented by the
+            // DownloadImageActivity.
+            // @@ DONE - you fill in here.
+            Intent downloadIntent = makeDownloadImageIntent(getUrl());
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            // Start the Activity associated with the Intent, which
+            // will download the image and then return the Uri for the
+            // downloaded image file via the onActivityResult() hook
+            // method.
+            // @@ DONE -- you fill in here.
+            startActivityForResult(downloadIntent, DOWNLOAD_IMAGE_REQUEST);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    /**
+     * Hook method called back by the Android Activity framework when
+     * an Activity that's been launched exits, giving the requestCode
+     * it was started with, the resultCode it returned, and any
+     * additional data from it.
+     */
+    @Override
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Check if the started Activity completed successfully.
+        // @@ DONE -- you fill in here, replacing true with the right
+        // code.
+        if (resultCode == RESULT_OK) {
+            // Check if the request code is what we're expecting.
+            // @@ DONE -- you fill in here, replacing true with the
+            // right code.
+            if (requestCode == DOWNLOAD_IMAGE_REQUEST) {
+                // Call the makeGalleryIntent() factory method to
+                // create an Intent that will launch the "Gallery" app
+                // by passing in the path to the downloaded image
+                // file.
+                // @@ DONE -- you fill in here.
+                Intent showImageIntent = makeGalleryIntent(data.getData().getPath());
+
+                // Start the Gallery Activity.
+                // @@ DONE -- you fill in here.
+                startActivity(showImageIntent);
+            }
+        }
+        // Check if the started Activity did not complete successfully
+        // and inform the user a problem occurred when trying to
+        // download contents at the given URL.
+        // @@ DONE -- you fill in here, replacing true with the right
+        // code.
+        else if (requestCode == RESULT_CANCELED) {
+            Toast.makeText(this, "An error occurred while trying to download" +
+                    "the file.", Toast.LENGTH_SHORT).show();
+        }
+    }    
+
+    /**
+     * Factory method that returns an Intent for viewing the
+     * downloaded image in the Gallery app.
+     */
+    private Intent makeGalleryIntent(String pathToImageFile) {
+        // Create an intent that will start the Gallery app to view
+        // the image.
+    	// MAYBE -- you fill in here, replacing "null" with the proper
+    	// code.
+        Intent galleryIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(pathToImageFile));
+        return galleryIntent;
+    }
+
+    /**
+     * Factory method that returns an Intent for downloading an image.
+     */
+    private Intent makeDownloadImageIntent(Uri url) {
+        // Create an intent that will download the image from the web.
+    	// MAYBE -- you fill in here, replacing "null" with the proper
+    	// code.
+        Intent downloadImageIntent = new Intent(Intent.ACTION_VIEW);
+        downloadImageIntent.setData(url);
+        return downloadImageIntent;
+    }
+
+    /**
+     * Get the URL to download based on user input.
+     */
+    protected Uri getUrl() {
+        Uri url = null;
+
+        // Get the text the user typed in the edit text (if anything).
+        url = Uri.parse(mUrlEditText.getText().toString());
+
+        // If the user didn't provide a URL then use the default.
+        String uri = url.toString();
+        if (uri == null || uri.equals(""))
+            url = mDefaultUrl;
+
+        // Do a sanity check to ensure the URL is valid, popping up a
+        // toast if the URL is invalid.
+        // @@ MAYBE -- you fill in here, replacing "true" with the
+        // proper code.
+        if (URLUtil.isHttpUrl(url.toString())) {
+            return url;
+        }
+        else {
+            Toast.makeText(this,
+                           "Invalid URL",
+                           Toast.LENGTH_SHORT).show();
+            return null;
+        } 
+    }
+
+    /**
+     * This method is used to hide a keyboard after a user has
+     * finished typing the url.
+     */
+    public void hideKeyboard(Activity activity,
+                             IBinder windowToken) {
+        InputMethodManager mgr =
+            (InputMethodManager) activity.getSystemService
+            (Context.INPUT_METHOD_SERVICE);
+        mgr.hideSoftInputFromWindow(windowToken,
+                                    0);
     }
 }
